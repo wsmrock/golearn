7c4dcdc8d473b36583dbe52035d6806c6f462250
wsmrock <31736414+wsmrock@users.noreply.github.com>
2018-12-07 16:12:51 +0800

    Update main.go

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     http.HandleFunc("/", sayhelloName) //设置访问的路由
-    err := http.ListenAndServe(":11111", nil) //设置监听的端口
+    err := http.ListenAndServe(":11114", nil) //设置监听的端口
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
     }
